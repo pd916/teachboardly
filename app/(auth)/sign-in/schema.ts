87bd0730a8702d6946166885d62e7af22787bf23
pd <pdbaginz@gmail.com>
2025-09-02 15:01:28 +0500

    schema  problem solve in sign-in

---
+++ app/(auth)/sign-in/schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string()
+    .min(6, "Password must be at least 6 characters")
+    .max(18, "Password can't exceed 100 characters"),
+});
