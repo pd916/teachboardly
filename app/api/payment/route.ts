1f71c6972874664de96211e85028053201bd9caf
pd <pdbaginz@gmail.com>
2025-12-25 14:57:04 +0500

    small updaton

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -4,22 +4,21 @@ import { NextResponse } from "next/server"
 import Stripe from "stripe"
 
 
+export const stripe = new Stripe(process.env.STRIPE_CLIENT_SECRET!, { apiVersion: "2025-12-15.clover" });
 
 export async function GET() {
     const self = await getSelf()
     if(!self) return NextResponse.json({status:404})
 
-    const stripeSecret = process.env.STRIPE_CLIENT_SECRET 
     const priceId = process.env.STRIPE_PRICE_ID
 
-    if (!stripeSecret || !priceId) {
+    if (!priceId) {
     return NextResponse.json(
       { error: "Stripe secret or price ID not configured" },
       { status: 500 }
     );
   }
 
-   const stripe = new Stripe(stripeSecret, { apiVersion: "2025-12-15.clover" });
 
     const session = await stripe.checkout.sessions.create({
         mode:'subscription',
